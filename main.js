d8e5b0c480991acdd2f870ba20545709086ae9e4
AidenKooG <k037686@gmail.com>
2023-02-22 20:51:07 +0900

    [Bugfix] Fix window.scrollY mistake

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-  console.log(window.scrollY());
+  console.log(window.scrollY);
   console.log(`navbarHeight: ${navbarHeight}`);
-});
+}); 
